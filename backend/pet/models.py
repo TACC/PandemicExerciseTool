033aa55ea31f29d1fadb623599fd33b14576daf2
wjallen <wallen@tacc.utexas.edu>
2024-08-29 00:55:49 -0500

    isolating worker into own container

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -12,9 +12,12 @@ class PET(models.Model):
     chi = models.FloatField()
     rho = models.FloatField()
     nu = models.CharField(max_length=4, choices={'high':'high', 'low':'low'})
+
     vaccine_wastage_factor = models.IntegerField()
     antiviral_effectiveness = models.FloatField()
     antiviral_wastage_factor = models.IntegerField()
 
+    phas = models.TextField(null=True)
+
     def __str__(self):
-        return(f'{self.disease_name}, R0={self.reproduction_number}, nu={self.nu}') 
+        return(f'{self.disease_name}, R0={self.R0}, nu={self.nu}') 
