5a0a2674a7445c31a42cb55e0d684458d7250604
Joseph Hendrix <joeylhendrix@gmail.com>
2024-10-01 17:31:44 -0500

    added a GET request to /api/reset when reset button is clicked; output in backend now clears with frontend localStorage

--- frontend/src/components/NewSimulationButton.js
+++ frontend/src/components/NewSimulationButton.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NewSimulationModal from './NewSimulationModal.js';
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
+import axios from 'axios';
 
 // create a button that spawns a modal window
 const NewSimulationButton = () => {
@@ -9,13 +10,12 @@ const NewSimulationButton = () => {
 
   const toggleModal = () => setIsModalOpen(!isModalOpen);
 
-  const handleReset = () => {
-    // clear parameters from local storage
+  const handleReset = async () => {
     console.log("clearing local storage");
     localStorage.clear();
-    // this is where we'll pass the GET request
     console.log("GETting reset request to Django server")
-    // refresh the page
+    // TODO: it might be prudent to wait for a 200 before refreshing the page
+    const response = await axios.get(`http://localhost:8000/api/reset`);
     location.reload();
   }
 
@@ -28,8 +28,8 @@ const NewSimulationButton = () => {
             <div className="modal-overlay" onClick={toggleModal}>
               <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <span className="modal-close" onClick={toggleModal}>×</span>
-                <h2>Warning!</h2>
-                <p>Starting a new simulation will clear all previous parameters and erase previous simulation data. Do you wish to continue?</p>
+                <h2 className="reset-warning">Warning!</h2>
+                <p>Starting a new simulation will clear all previous parameters and erase previous simulation data.</p>
                 <div className="confirmation-buttons">
                   <button className="cancel-reset-button" onClick={toggleModal}>Cancel</button>
                   <button className="confirm-reset-button" onClick={handleReset}>Reset</button>
