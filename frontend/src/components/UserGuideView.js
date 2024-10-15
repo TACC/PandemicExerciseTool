21638566aadc86e966d4cb9460bb170b001b8cc4
gladyschenyx <98440939+gladyschenyx@users.noreply.github.com>
2024-10-15 09:01:26 -0500

    User Guide Updates

--- frontend/src/components/UserGuideView.js
+++ frontend/src/components/UserGuideView.js
@@ -190,7 +190,7 @@ const UserGuideView = () => {
     <div className="user-guide-view">
       <section className="text">
         <h2>Interactive Outbreak Simulator</h2>
-        <p>Updated as of 10/11/2024</p>
+        <h3>Updated as of 10/11/2024</h3>
         <a href="https://github.com/TACC/PandemicExerciseSimulator" target="_blank" rel="noopener noreferrer" className="github-link">
         https://github.com/TACC/PandemicExerciseSimulator
         </a>
@@ -223,24 +223,24 @@ const Instructions = () => {
   return (
     <div className="instructions">
       <section className="text">
-        <h3>Using the Simulator</h3>
         <p>Set the scenario with the 'Set Scenario' button in the top-left corner of the screen</p>
         <img src={ require('./images/userGuide-setScenario.png') } alt="Setting the Scenario" />
         <p>Select 'Disease Parameters' from the dropdown menu to specify starting conditions for the outbreak</p>
         <ul className="bullet-points">
-          <li>R0: average number of secondary infections in a susepctible population</li>
+          <li>R0: average number of secondary infections in a susceptible population</li>
           <li>Latency period (days): average number of days spent asymptomatic immediately after infection</li>
           <li>Asymptomatic period (days): average number of days spent infectious, but not yet symptomatic</li>
-          <li>Symptomatic period (days): average number of days spent symptomatic and infections</li>
-          <li>Asymptomatic rate (proportion): proportion of infections that never develop symptoms</li>
+          <li>Symptomatic period (days): average number of days spent symptomatic and infectious</li>
+          <li>Asymptomatic rate (proportion): proportion of infections that never developed symptoms</li>
           <li>Infection fatality rate (proportion): proportion of infections that lead to death</li>
         </ul>
         <img src={ require('./images/userGuide-parameters1.png') } alt="Selecting Parameters" />
         <img src={ require('./images/userGuide-parameters2.png') } alt="Inputting Parameters" />
         <p>Load a preset scenario from the catalog and hit the 'Save' button</p>
         <img src={ require('./images/userGuide-presetSave.png') } alt="Saving Parameters" />
-        <p>Select 'Initial Cases' from the dropdown menu to specify initial infections. Click the '+ Add Cases' button to confirm</p>
+        <p>Select 'Initial Cases' from the dropdown menu to specify initial infections </p>
         <img src={ require('./images/userGuide-InitialCases1.png') } alt="Saving Parameters" />
+        <p>Click the '+ Add Cases' button to add initial cases for a particular county</p>
         <img src={ require('./images/userGuide-InitialCases2.png') } alt="Saving Parameters" />
         <p>For instance, enter the following entries:</p>
         <ul className="bullet-points">
@@ -256,11 +256,11 @@ const Instructions = () => {
         </ul>
         <img src={ require('./images/userGuide-InitialCases4.png') } alt="Additional Initial Cases" />
         <p>After adding cases, click 'Save and Close' to submit initial cases</p>
-        <p>Saved scenario parameters and initial cases are summarized in the left-hand panel</p>
+        <p>Saved scenario parameters and initial cases will be summarized in the left-hand panel</p>
         {/* <img src={ require('./images/userGuide-summaryPanel.png') } alt="Summary Panel" /> */}
         <p>Click the 'Play' button in the bottom-left corner to run the simulation</p>
         <img src={ require('./images/userGuide-playScenario.png') } alt="Running the Simulation" style={{ width: '50%' }} />
-        <p>Hovering over the map will display infection data for a given county</p>
+        <p>Hovering over the map will display the infections for specific counties</p>
         <img src={ require('./images/userGuide-onHover.png') } alt="Hovering over Counties" style={{ width: '50%' }}/>
         <p>The infection map can be toggled to display the number of infected individuals per county</p>
         <img src={ require('./images/userGuide-countToggle.png') } alt="Toggling Absolute Counts" style={{ width: '50%' }}/>
@@ -269,8 +269,8 @@ const Instructions = () => {
         <p>Clicking on a compartment in the legend will toggle that compartment on or off in the line chart</p>
         <img src={ require('./images/userGuide-toggleCompartments.png') } alt="Toggling Compartments" style={{ width: '50%' }}/>
         <p>
-          The table on the right-hand side of the screen can be sorted alphabetically by county or by 
-          ascending/descending order by infections or deaths per county
+          The table on the right-hand side of the screen can be sorted alphabetically by county or in 
+          ascending/descending order based on infections or deaths
         </p>
         <p>
           Use the search bar above the table to search for specific counties
@@ -288,18 +288,18 @@ const Instructions = () => {
         <p>Interventions are added using the '+ Add Interventions' button beneath the 'Set Scenario' button</p>
         <img src={ require('./images/userGuide-setScenario.png') } alt="Setting the Scenario" />
         <p>
-          Select 'Non-Pharmaceutical' from the dropdown menu to add non-pharmaceutical interventions, or NPIs.
-          Click the 'Add New NPI' button to confirm NPI parameters
+          Select 'Non-Pharmaceutical' from the dropdown menu to add non-pharmaceutical interventions, or NPIs
         </p>
+        <img src={ require('./images/userGuide-NPIs1.png') } alt="Selecting NPIs" />
+        <p> Click the 'Add New NPI' button to confirm NPI parameters</p>
         <ul className="bullet-points">
           <li>NPI start (simulation day): day of simulation on which the NPI is initiated</li>
           <li>NPI duration (days): the number of days the NPI is active</li>
           <li>
-            NPI effectiveness (proportion): age-specific reduction in susceptibility (
-            0 = no protection; 1 = complete protection)
+            NPI effectiveness (proportion): age-specific reduction in susceptibility 
+            (0 = no protection; 1 = complete protection)
           </li>
         </ul>
-        <img src={ require('./images/userGuide-NPIs1.png') } alt="Selecting NPIs" />
         <img src={ require('./images/userGuide-NPIs2.png') } alt="Selecting NPIs" />
         <p>After adding, click 'Save' to submit NPIs to the simulator</p>
         <img src={ require('./images/userGuide-NPIs3.png') } alt="Saving NPIs" />
