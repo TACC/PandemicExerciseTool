96db32e531abfe763c921855f6ea4b0a44fcfa02
Joseph Hendrix <JLH7459@wireless-10-145-18-24.public.utexas.edu>
2024-10-25 15:56:33 -0500

    notification works but keeps reappearing as simulation progresses

--- frontend/src/components/Home/PlayPauseButton.js
+++ frontend/src/components/Home/PlayPauseButton.js
@@ -5,6 +5,7 @@ import React from 'react';
 import './PlayPauseButton.css';
 import play_button from '../images/play_button.png';
 import pause from '../images/pause.png';
+import StartNotification from './StartNotification';
 
 const checkForScenario = () => {
   if (localStorage.length > 2) {
@@ -19,24 +20,25 @@ const checkForScenario = () => {
     initialCasesSet = JSON.parse(localStorage.getItem('initial_infected')).length > 0
   ) : (
     initialCasesSet = false);
-  console.log("parametersSet:", parametersSet);
-  console.log("initialCasesSet:", initialCasesSet);
   return [parametersSet, initialCasesSet];
 };
 
 const PlayPauseButton = ({ isRunning, onToggle }) => {
   const hasSetParameters = checkForScenario();
+
   if (hasSetParameters[0] && hasSetParameters[1]) {
     return (
-      <button className={isRunning ? "pause-button" : "scenario-button"} onClick={onToggle}>
-        <img src={isRunning ? pause : play_button} alt={isRunning ? "Pause" : "Play"} className="icon" />
-        {isRunning ? "Pause" : "Play"}
-      </button>
-    );
+      <div>
+        <StartNotification isRunning={isRunning} />
+        <button className={isRunning ? "pause-button" : "scenario-button"} onClick={onToggle}>
+          <img src={isRunning ? pause : play_button} alt={isRunning ? "Pause" : "Play"} className="icon" />
+          {isRunning ? "Pause" : "Play"}
+        </button>
+      </div>
+    )
   } else {
     return (
       <>
-      {/* <span className="tooltips-text">Scenario parameters and initial cases are required to run a simulation</span> */}
       <button className="scenario-button-disabled">
         <span className="tooltips-text">Scenario parameters and initial cases are required to run a simulation</span>
         <img src={play_button} alt="Play button disabled, missing parameters" className="icon" />
--- frontend/src/components/Home/StartNotification.js
+++ frontend/src/components/Home/StartNotification.js
@@ -1,15 +1,14 @@
 // displays a notification when the user starts a simulation
 // TODO: more docstrings here
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Toast from 'react-bootstrap/Toast';
-import Overlay from 'react-bootstrap/Overlay';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 
-const StartNotification = (isRunning, target) => {
-  const [show, setShow] = useState(true);
-  console.log("ref.current:", target.current);
+const StartNotification = (isRunning) => {
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     console.log("isRunning?", isRunning.isRunning);
@@ -17,33 +16,18 @@ const StartNotification = (isRunning, target) => {
   }, [isRunning])
 
   return (
-    <>
-      <Overlay target={target.current} show={show} placement="right">
-        {({
-          placement: _placement,
-          arrowProps: _arrowProps,
-          show: _show,
-          popper: _popper,
-          hasDoneInitialMeasure: _hasDoneInitialMeasure,
-          ...props
-        }) => (
-          <div
-            {...props}
-            style={{
-              position: 'absolute',
-                backgroundColor: 'white',
-                padding: '2px 10px',
-                color: 'black',
-                borderRadius: 4,
-                ...props.style,
-              }}
-          >
-            Starting simulation...
-          </div>
-        )}
-      </Overlay>
-    </>
-  );
+      <Col xs={6}>
+        <ToastContainer 
+          className="notification-container" 
+          // position={'bottom-center'}
+          style = {{zIndex: '4', left: '11.6em !important', bottom: '85%', width: '14.2em'}}
+        >
+          <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+            <Toast.Body>Starting simulation...</Toast.Body>
+          </Toast>
+        </ToastContainer>
+      </Col>
+  )
 }
 
 export default StartNotification;
