4f84a530ab9155e8f53998eaa9d8dd65e601685a
Joe Allen <wallen@tacc.utexas.edu>
2026-02-02 01:01:41 -0600

    update data model to incorporate vaccine correctly

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -5,6 +5,8 @@ class PET(models.Model):
 
     disease_name = models.TextField()
     model_type = models.TextField()
+    state=models.TextField()
+
     R0 = models.FloatField()
     beta_scale = models.FloatField()
     tau = models.FloatField()
@@ -13,26 +15,25 @@ class PET(models.Model):
     chi = models.FloatField()
     rho = models.FloatField()
     nu = models.TextField()
-    #sigma = models.TextField()
-
+    sigma = models.TextField()
     infectious_period = models.FloatField()
     immune_period = models.FloatField()
-
-    state=models.TextField()
 
     initial_infected = models.TextField(null=True)
     npis = models.TextField(null=True)
 
-    antiviral_effectiveness = models.FloatField(null=True)
-    antiviral_wastage_factor = models.FloatField(null=True)
-    antiviral_stockpile = models.TextField(null=True)
+    #antiviral_effectiveness = models.FloatField(null=True)
+    #antiviral_wastage_factor = models.FloatField(null=True)
+    #antiviral_stockpile = models.TextField(null=True)
 
-    vaccine_wastage_factor = models.FloatField(null=True)
-    vaccine_pro_rata = models.CharField(max_length=8, choices={'children':'children', 'pro_rata':'pro_rata'}, null=True)
-    vaccine_adherence = models.TextField(null=True)
-    vaccine_effectiveness = models.TextField(null=True)
-    vaccine_stockpile = models.TextField(null=True)
+    vaccine_model = models.TextField(blank=True)
+    vaccine_priority_groups = models.TextField(blank=True)
+    vaccine_capacity = models.FloatField(blank=True)
+    vaccine_effectiveness_lag = models.FloatField(blank=True)
+    vaccine_effectiveness = models.TextField(blank=True)
+    vaccine_adherence = models.TextField(blank=True)
+    vaccine_stockpile = models.TextField(blank=True)
     
     def __str__(self):
-        return(f'{self.disease_name}, R0={self.R0}, nu={self.nu}') 
+        return(f'{self.disease_name}, model_type={self.model_type}, state={self.state}') 
 
