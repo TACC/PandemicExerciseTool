dd45f6d1513fed31027a7411a58112746432c359
Emily Javan, MS PhD <emjavan@gmail.com>
2026-02-03 09:18:40 -0600

    make backend day limits match

--- backend/pet/pes_task.py
+++ backend/pet/pes_task.py
@@ -265,7 +265,7 @@ def run_pes(input):
                       '--input',
                       '/PES/INPUT.json',
                       '--days',
-                      '999',
+                      '200',
                       '--loglevel',
                       'INFO'],
                       preexec_fn=on_parent_exit('SIGHUP'))
